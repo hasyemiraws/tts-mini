aa7b116e38913928c3f41efd531d948c1cfffc1d
Hasyemi Rafsanjani Asyari <hasyemirafsanjaniasyari@gmail.com>
2023-02-12 15:08:06 +0700

    feat(page): main screen

---
+++ src/components/MainLogo.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="flex flex-col">
+        <div class="flex tile-group rounded-[2px]">
+            <div class="tile"><span class="tile-number">1.</span>T</div>
+            <div class="tile"><span class="tile-number">2.</span>T</div>
+            <div class="tile"><span class="tile-number">3.</span>S</div>
+        </div>
+        <div class="flex mt-1 -ml-1">
+            <div class="sub-tile">m</div>
+            <div class="sub-tile">i</div>
+            <div class="sub-tile">n</div>
+            <div class="sub-tile">i</div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.tile-group {
+    box-shadow: -2px 2px 0px #000000;
+}
+
+.tile {
+    width: 85px;
+    height: 85px;
+    font-size: 48px;
+    @apply flex items-center justify-center bg-white border-2 border-black font-semibold relative
+}
+
+.tile:nth-child(1) {
+    border-top-left-radius: 2px;
+    border-bottom-left-radius: 2px;
+}
+
+.tile:nth-child(3) {
+    border-top-right-radius: 2px;
+    border-bottom-right-radius: 2px;
+}
+
+.tile+.tile {
+    border-left: 0;
+}
+
+.tile-number {
+    position: absolute;
+    top: 4px;
+    left: 4px;
+    font-size: 8px;
+}
+
+.sub-tile {
+    font-size: 24px;
+    padding: 10px;
+    @apply flex flex-1 items-center justify-center bg-black font-semibold relative border border-white text-white rounded
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { RouterLink } from 'vue-router';
+import MainLogo from '../components/MainLogo.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="h-screen overflow-y-hidden flex flex-col items-center justify-center">
+    <MainLogo/>
+    <p class="mt-20 mb-10 italic">
+      Teka-teki silang mini Bahasa Indonesia.<br/>
+      Setiap harinya ada teka-teki berbeda.
+    </p>
+    <RouterLink class="rounded bg-black w-60 p-3 text-white flex items-center justify-center text-sm uppercase font-medium" to="/about">Mulai</RouterLink>
   </main>
 </template>
